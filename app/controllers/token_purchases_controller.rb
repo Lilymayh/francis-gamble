bf3d3b0332ccd32766ae483777e3dcdc1cee9dbb
Lily <lmayhenderson@yahoo.com>
2024-05-13 16:51:29 -0700

    Add routes, controller, and views page for purchasing tokens.

---
+++ app/controllers/token_purchases_controller.rb
@@ -0,0 +1,10 @@
+class TokenPurchasesController < ApplicationController
+  
+  def new_token_purchase
+  end
+
+  def create_token_purchase
+    #save purchase and redirect to home page
+  end
+
+end
