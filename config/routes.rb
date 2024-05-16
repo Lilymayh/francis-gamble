bd4b368502077bdb9b46e3a761b66fa6fc947ba5
Lily <lmayhenderson@yahoo.com>
2024-05-15 21:38:26 -0700

    Start checkers board

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/tokens' => 'token_purchases#new_token_purchase'
   post '/tokens' => 'token_purchases#create_token_purchase'
 
+  get '/pages/checkers' => 'pages#checkers'
+
 end
