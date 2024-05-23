67eda8d43949ce04eb74c9a32db9a3e55a5c5dd9
animward <animward@gmail.com>
2024-05-23 02:55:53 -0700

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,21 @@ class SessionsController < ApplicationController
   def new
   end
 
-	def create
+  def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-  session[:users_id] = user.id
+      session[:user_id] = user.id
       redirect_to "/games"
     else
-      redirect_to "/"
+      flash.now[:alert] = "Invalid email/password combination"
     end
   end
+  
+
+
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path, notice: "Logged out successfully"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/register', to: 'users#create'
-  delete '/logout', to: 'sessions#destroy'
 
+  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
 
   resources :tictactoe, only: [:create, :show, :update, :index, :new] do
