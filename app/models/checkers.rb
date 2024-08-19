c1533927bec717cec2ef94d31c20992a5a05252c
Lily <lmayhenderson@yahoo.com>
2024-08-19 12:29:04 -0700

    Few tweaks before switching branches.

--- app/models/checkers.rb
+++ app/models/checkers.rb
@@ -1,3 +1,11 @@
 class Checkers < ApplicationRecord
+	has_one :game_board, dependent: :destroy
+  after_create :initialize_game_board
 
+  private
+
+  def initialize_game_board
+    create_game_board unless game_board
+    # Initialize squares or any other necessary setup
+  end
 end
